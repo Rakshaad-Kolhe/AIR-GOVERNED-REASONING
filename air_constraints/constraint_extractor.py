8f8b131ab12b51f417762cf89f600298272d8d37
Rakshaad-Kolhe <rakshaadkolhe@gmail.com>
2026-03-12 23:46:13 +0530

    Initial commit

---
+++ air_constraints/constraint_extractor.py
@@ -0,0 +1,10 @@
+﻿class ConstraintExtractor:
+    def extract(self, step, failure):
+        if failure is None:
+            return None
+
+        return {
+            "premises": step.premises,
+            "operator": step.operator,
+            "required_relation": failure.get("error_type"),
+        }
