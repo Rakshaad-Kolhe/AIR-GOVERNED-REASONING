6ce340fdc7c3c8c95d9bb4bdf7f7850a27fd0cfe
Rakshaad-Kolhe <rakshaadkolhe@gmail.com>
2026-03-12 23:43:01 +0530

    Initial commit

---
+++ air_constraints/constraint_library.py
@@ -0,0 +1,26 @@
+﻿class ConstraintLibrary:
+    def __init__(self):
+        self.constraints = []
+
+    def add(self, constraint):
+        if constraint is not None:
+            self.constraints.append(constraint)
+
+    def get_all(self):
+        return list(self.constraints)
+
+    def match(self, premises):
+        matches = []
+        input_premises = premises or []
+
+        for constraint in self.constraints:
+            constraint_premises = []
+            if isinstance(constraint, dict):
+                constraint_premises = constraint.get("premises") or []
+
+            for premise in input_premises:
+                if premise in constraint_premises:
+                    matches.append(constraint)
+                    break
+
+        return matches
