03c59b2e2cb312ebaebeb52ad9276d62db78a4ed
Rakshaad-Kolhe <rakshaadkolhe@gmail.com>
2026-03-13 14:26:55 +0530

    Initial Commit

--- air_training/signal_generator.py
+++ air_training/signal_generator.py
@@ -5,10 +5,12 @@ class TrainingSignalGenerator:
 
         failure = result.get("failure")
         repair = result.get("repair")
+        constraint = result.get("constraint")
 
         return {
             "question": question,
             "failure_type": failure.get("error_type") if failure else None,
             "repair_suggestion": repair.get("suggestion") if repair else None,
             "trace": result.get("reasoning_trace"),
+            "constraint": constraint,
         }
